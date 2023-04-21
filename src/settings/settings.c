3b00871cd355a2e1bf5a66fdf42019590c679006
Alxv <avong2011@gmail.com>
2023-04-21 09:27:44 -0400

    style: added volumn and brightness controls into settings

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -18,37 +18,73 @@
 #define MENU_ITEMS 4
 static char *menuItems[MENU_ITEMS];
 
-void settingsMenu(SDL_Surface *surface, int selected, int i) {
+void settingsMenu(SDL_Surface *surface, int selected, int value, int minValue, int maxValue, int i) {
     #define MIN(a, b) (a) < (b) ? (a) : (b)
     char *item = menuItems[i];
-    int margin_left = 32;
-
+    int marginLeft = 32;
+    int background = selected == SETTINGS_VOLUMN || selected == SETTINGS_SCREEN? SDL_MapRGB(surface->format, TRIAD_GRAY) : SDL_MapRGB(surface->format, TRIAD_WHITE);
     SDL_Surface *text;
     text = TTF_RenderUTF8_Blended(g_font.small, item, COLOR_LIGHT_TEXT);
-    int row_width = text->w + margin_left * 2;
+    SDL_Surface *sIcon = i == SETTINGS_SCREEN ? g_gfx.brightness : (i == SETTINGS_VOLUMN ? g_gfx.volume : g_gfx.mute);
+    SDL_Surface *sProgressEmpty = g_gfx.settings_bar_empty;
+    SDL_Surface *sProgressBar = g_gfx.settings_bar_full;
+    
+    int row_width = text->w + marginLeft * 2;
     int max_width = MIN(row_width, 580);
     int row_cy = (ROW_HEIGHT / 2) - (text->h / 2);
     int screen_center = (SCREEN_HEIGHT / 2) - ((ROW_HEIGHT * ROW_COUNT) / 2);
+    int iconMarginLeft = ICON_SIZE + 8;
+    int cy = (sIcon->h / 2) - (sProgressEmpty->h / 2);
+    int w = sProgressBar->w * ((float)(value - minValue) / (maxValue - minValue));
+    int h = sProgressBar->h;
 
     if (i == selected) {
-      // Selected rows
       text = TTF_RenderUTF8_Blended(g_font.small, item, COLOR_DARK_TEXT);
-      SDL_FillRect(surface,
-                   &(SDL_Rect){0, screen_center + i * ROW_HEIGHT, max_width,
-                               ROW_HEIGHT},
-                   SDL_MapRGB(surface->format, TRIAD_WHITE));
-      SDL_BlitSurface(
-          text, &(SDL_Rect){0, 0, max_width, text->h}, surface,
-          &(SDL_Rect){margin_left,
-                      screen_center + (i * ROW_HEIGHT) + row_cy});
-      SDL_FreeSurface(text);
+      SDL_FillRect(surface, &(SDL_Rect){0, screen_center + i * ROW_HEIGHT, max_width,  ROW_HEIGHT}, background);
+      if (selected == SETTINGS_VOLUMN || selected == SETTINGS_SCREEN) {
+        SDL_BlitSurface(sIcon, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+        SDL_BlitSurface(sProgressEmpty, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+        SDL_BlitSurface(sProgressBar, &(SDL_Rect){0, 0, w, h}, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy, w, h});
+      } else {
+        SDL_BlitSurface(text, &(SDL_Rect){0, 0, max_width, text->h}, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+        SDL_FreeSurface(text);
+      }
+    } else if (i == SETTINGS_VOLUMN) {
+      SDL_BlitSurface(sIcon, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+      SDL_BlitSurface(sProgressEmpty, NULL, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+      SDL_BlitSurface(sProgressBar, &(SDL_Rect){0, 0, w, h}, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy, w, h});
+    } else if (i == SETTINGS_SCREEN) {
+      SDL_BlitSurface(sIcon, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+      SDL_BlitSurface(sProgressEmpty, NULL, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+      SDL_BlitSurface(sProgressBar, &(SDL_Rect){0, 0, w, h}, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy, w, h});
     } else {
-      SDL_BlitSurface(
-          text, &(SDL_Rect){0, 0, max_width, text->h}, surface,
-          &(SDL_Rect){margin_left,
-                      screen_center + (i * ROW_HEIGHT) + row_cy});
+      SDL_BlitSurface(text, &(SDL_Rect){0, 0, max_width, text->h}, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
       SDL_FreeSurface(text);
     }
+
+    // if (i == selected) {
+    //   // Selected rows
+    //   text = TTF_RenderUTF8_Blended(g_font.small, item, COLOR_DARK_TEXT);
+    //   SDL_FillRect(surface, &(SDL_Rect){0, screen_center + i * ROW_HEIGHT, max_width,  ROW_HEIGHT}, SDL_MapRGB(surface->format, selected==SETTINGS_VOLUMN || SETTINGS_SCREEN? TRIAD_GRAY:TRIAD_WHITE));
+
+    //   if (selected == SETTINGS_VOLUMN || SETTINGS_SCREEN) {
+    //     SDL_BlitSurface(sIcon, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_BlitSurface(sProgressEmpty, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_BlitSurface(sProgressBar, &(SDL_Rect){0, 0, w, h}, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy, w, h});
+    //   } else {
+    //     SDL_BlitSurface(text, &(SDL_Rect){0, 0, max_width, text->h}, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_FreeSurface(text);
+    //   }
+    // } else {
+    //   if (selected == SETTINGS_VOLUMN || SETTINGS_SCREEN) {
+    //     SDL_BlitSurface(sIcon, NULL, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_BlitSurface(sProgressEmpty, NULL, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_BlitSurface(sProgressBar, &(SDL_Rect){0, 0, w, h}, surface, &(SDL_Rect){marginLeft + iconMarginLeft, screen_center + (i * ROW_HEIGHT) + row_cy, w, h});
+    //   } else {
+    //     SDL_BlitSurface(text, &(SDL_Rect){0, 0, max_width, text->h}, surface, &(SDL_Rect){marginLeft, screen_center + (i * ROW_HEIGHT) + row_cy});
+    //     SDL_FreeSurface(text);
+    //   }
+    // }
 }
 
 void initSettings(SDL_Surface *surface) {
@@ -57,17 +93,17 @@ void initSettings(SDL_Surface *surface) {
   menuItems[SETTINGS_SCREEN] = "Screen Brightness";
   menuItems[SETTINGS_VOLUMN] = "Adjust volumn";
   // int background = SDL_MapRGB(surface->format, TRIAD_BLACK);
-
+  InitSettings();
   int selected = 0;
   int dirty = 1;
   int quit = 0;
   int showSettings = 0;
   // Device settings
-  int show_setting = 0; // 1=brightness,2=volume
-	int setting_value = 0;
-	int setting_min = 0;
-	int setting_max = 0;
+	int volBritValue = 0;
+	int volBritMin = 0;
+	int volBritMax = 0;
   ///
+
 
   while(!quit) {
     SDL_FillRect(surface, NULL, 0);
@@ -102,12 +138,25 @@ void initSettings(SDL_Surface *surface) {
           break;
         }
         //TODO: add status switch case to track options
+    } else if (Input_justPressed(BTN_MINUS) || Input_justPressed(BTN_PLUS)) {
+        switch(selected) {
+        case SETTINGS_SCREEN:
+        volBritValue = GetBrightness();
+        volBritMax = MIN_BRIGHTNESS;
+        volBritMax = MAX_BRIGHTNESS;
+        break;
+        case SETTINGS_VOLUMN:
+        volBritValue = GetVolume();
+        volBritMin = MIN_VOLUME;
+        volBritMax = MAX_VOLUME;
+        break;
+        }
     }
 
     // Dirty
     if(dirty) {
       for (int i = 0; i < MENU_ITEMS; i++) {
-        settingsMenu(surface, selected, i);
+        settingsMenu(surface, selected, volBritValue, volBritMin, volBritMax, i);
 	      // SDL_FillRect(surface, NULL, background);
       }
       button(surface, "A", "Select", 0, 1, SCREEN_WIDTH - PADDING_LR, 419);
